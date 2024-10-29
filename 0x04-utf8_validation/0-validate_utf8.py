7d6c1674ee231c7db93dd5c77358da649f1ffe14
Benny254 <asshimbenny01@gmail.com>
2024-10-29 09:27:41 +0000

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,18 +12,18 @@ def validUTF8(data):
     """
     byte_count = 0
 
-    for i in data:
+    for r in data:
         if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+            if r >> 5 == 0b110 or r >> 5 == 0b1110:
                 byte_count = 1
-            elif i >> 4 == 0b1110:
+            elif r >> 4 == 0b1110:
                 byte_count = 2
-            elif i >> 3 == 0b11110:
+            elif r >> 3 == 0b11110:
                 byte_count = 3
-            elif i >> 7 == 0b1:
+            elif r >> 7 == 0b1:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if r >> 6 != 0b10:
                 return False
             byte_count -= 1
     return byte_count == 0
