c8dbfaff7e1033b4de3b90d99462c7b1a460e4dc
Benny254 <asshimbenny01@gmail.com>
2024-11-19 07:47:38 +0000

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,55 +1,32 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
-        """Rotates an n x n 2D matrix 90 degrees clockwise in place.
-            """
-                if not isinstance(matrix, list) or not matrix:
-                            return
-                            n = len(matrix)
-                                if not all(isinstance(row, list) and len(row) == n for row in matrix):
-                                            return
+        """
+            Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+                """
+                    n = len(matrix)
+                        for layer in range(n // 2):
+                                    first, last, offset = layer, n - 1 - layer, 0
+                                            for i in range(first, last):
+                                                            top = matrix[first][i]
+                                                                        matrix[first][i] = matrix[last - offset][first]
+                                                                                    matrix[last - offset][first] = matrix[last][last - offset]
+                                                                                                matrix[last][last - offset] = matrix[i][last]
+                                                                                                            matrix[i][last] = top
+                                                                                                                        offset += 1
 
-                                            for layer in range(n // 2):
-                                                        first, last = layer, n - layer - 1
-                                                                for i in range(first, last):
-                                                                                offset = i - first
-                                                                                            # Save the top element
-                                                                                                        top = matrix[first][i]
-                                                                                                                    # Move left element to top
-                                                                                                                                matrix[first][i] = matrix[last - offset][first]
-                                                                                                                                            # Move bottom element to left
-                                                                                                                                                        matrix[last - offset][first] = matrix[last][last - offset]
-                                                                                                                                                                    # Move right element to bottom
-                                                                                                                                                                                matrix[last][last - offset] = matrix[i][last]
-                                                                                                                                                                                            # Assign top element to right
-                                                                                                                                                                                                        matrix[i][last] = top
-                                                                                                                                                                                                        #!/usr/bin/python3
-                                                                                                                                                                                                        """2D matrix rotation module.
-                                                                                                                                                                                                        """
+                                                                                                                        # def rotate_2d_matrix(matrix):
+                                                                                                                        #     n = len(matrix)
 
-                                                                                                                                                                                                        def rotate_2d_matrix(matrix):
-                                                                                                                                                                                                                """Rotates an n x n 2D matrix 90 degrees clockwise in place.
-                                                                                                                                                                                                                    """
-                                                                                                                                                                                                                        if not isinstance(matrix, list) or not matrix:
-                                                                                                                                                                                                                                    return
-                                                                                                                                                                                                                                    n = len(matrix)
-                                                                                                                                                                                                                                        if not all(isinstance(row, list) and len(row) == n for row in matrix):
-                                                                                                                                                                                                                                                    return
+                                                                                                                        #     # Transpose the matrix (swap rows with columns)
+                                                                                                                        #     for i in range(n):
+                                                                                                                        #         for j in range(i, n):
+                                                                                                                        #             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-                                                                                                                                                                                                                                                    for layer in range(n // 2):
-                                                                                                                                                                                                                                                                first, last = layer, n - layer - 1
-                                                                                                                                                                                                                                                                        for i in range(first, last):
-                                                                                                                                                                                                                                                                                        offset = i - first
-                                                                                                                                                                                                                                                                                                    # Save the top element
-                                                                                                                                                                                                                                                                                                                top = matrix[first][i]
-                                                                                                                                                                                                                                                                                                                            # Move left element to top
-                                                                                                                                                                                                                                                                                                                                        matrix[first][i] = matrix[last - offset][first]
-                                                                                                                                                                                                                                                                                                                                                    # Move bottom element to left
-                                                                                                                                                                                                                                                                                                                                                                matrix[last - offset][first] = matrix[last][last - offset]
-                                                                                                                                                                                                                                                                                                                                                                            # Move right element to bottom
-                                                                                                                                                                                                                                                                                                                                                                                        matrix[last][last - offset] = matrix[i][last]
-                                                                                                                                                                                                                                                                                                                                                                                                    # Assign top element to right
-                                                                                                                                                                                                                                                                                                                                                                                                                matrix[i][last] = top
-
+                                                                                                                        #     # Reverse the rows to complete the rotation
+                                                                                                                        #     for i in range(n):
+                                                                                                                        #         matrix[i].reverse()
