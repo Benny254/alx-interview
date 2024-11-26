0243b680628fdf10f6cad31b333462c3104bee5b
Benny254 <asshimbenny01@gmail.com>
2024-11-26 09:45:04 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,24 +2,27 @@
 """Change making module.
 """
 
-
 def makeChange(coins, total):
-        """Determines the fewest number of coins needed to meet a given
-            amount total when given a pile of coins of different values.
-                """
-                    if total <= 0:
-                                return 0
-                                rem = total
-                                    coins_count = 0
-                                        coin_idx = 0
-                                            sorted_coins = sorted(coins, reverse=True)
-                                                n = len(coins)
-                                                    while rem > 0:
-                                                                if coin_idx >= n:
-                                                                                return -1
-                                                                                    if rem - sorted_coins[coin_idx] >= 0:
-                                                                                                    rem -= sorted_coins[coin_idx]
-                                                                                                                coins_count += 1
-                                                                                                                        else:
-                                                                                                                                        coin_idx += 1
-                                                                                                                                            return coins_count
+        """Determines the fewest number of coins needed to meet a given amount total.
+            
+                Args:
+                        coins (list): A list of the values of the coins in your possession.
+                                total (int): The total amount to be met.
+
+                                    Returns:
+                                            int: The fewest number of coins needed to meet the total, or -1 if the total cannot be met.
+                                                """
+                                                    if total <= 0:
+                                                                return 0
+
+                                                                sorted_coins = sorted(coins, reverse=True)
+                                                                    coins_count = 0
+                                                                        remaining = total
+
+                                                                            for coin in sorted_coins:
+                                                                                        while remaining >= coin:
+                                                                                                        remaining -= coin
+                                                                                                                    coins_count += 1
+
+                                                                                                                        return coins_count if remaining == 0 else -1
+
