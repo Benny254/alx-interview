7f9b903a3e064b01e31b76ebc2261073896152ab
Benny254 <asshimbenny01@gmail.com>
2024-11-26 09:48:11 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""Change making module.
+"""
+Interview Question on: fewest number of coins needed to
+meet a given amount total
 """
 
+
 def makeChange(coins, total):
-        """Determines the fewest number of coins needed to meet a given amount total.
-            
-                Args:
-                        coins (list): A list of the values of the coins in your possession.
-                                total (int): The total amount to be met.
-
-                                    Returns:
-                                            int: The fewest number of coins needed to meet the total, or -1 if the total cannot be met.
-                                                """
-                                                    if total <= 0:
-                                                                return 0
-
-                                                                sorted_coins = sorted(coins, reverse=True)
-                                                                    coins_count = 0
-                                                                        remaining = total
-
-                                                                            for coin in sorted_coins:
-                                                                                        while remaining >= coin:
-                                                                                                        remaining -= coin
-                                                                                                                    coins_count += 1
-
-                                                                                                                        return coins_count if remaining == 0 else -1
-
+        """ fewest number of coins needed to meet total """
+            if total <= 0:
+                        return 0
+                        # sort the coins in descending order
+                            coins.sort(reverse=True)
+                                change = 0
+                                    for coin in coins:
+                                                if total <= 0:
+                                                                break
+                                                                    temp = total // coin
+                                                                            change += temp
+                                                                                    total -= (temp * coin)
+                                                                                        if total != 0:
+                                                                                                    return -1
+                                                                                                    return change
