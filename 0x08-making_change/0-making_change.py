392bbd30ca9d19d8a26a245a7aeec3ac46217367
Benny254 <asshimbenny01@gmail.com>
2024-11-27 09:04:27 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
+"""
+Task: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed to
+meet a given amount total
+"""
 
 
-def make_change(coins, total):
-        """Determines the fewest number of coins needed \
-                        to meet a given amount total"""
-                            if total <= 0:
-                                        return 0
-
-                                        current_total = 0
-                                            used_coins = 0
-                                                coins = sorted(coins, reverse=True)
-                                                    for coin in coins:
-                                                                r = (total - current_total) // coin
-                                                                        current_total += r * coin
-                                                                                used_coins += r
-                                                                                        if current_total == total:
-                                                                                                        return used_coins
-                                                                                                        return -1
+def makeChange(coins, total):
+        """
+            Given a pile of coins of different values, determine the fewest
+                number of coins needed to meet a given amount total.
+                    Return: fewest number of coins needed to meet total
+                            - If total is 0 or less, return 0
+                                    - If total cannot be met by any number of coins you have, return -1
+                                            - Coins is a list of the values of the coins in your possession
+                                                    - The value of a coin will always be an integer greater than 0
+                                                            - You can assume you have an infinite number of each denomination of
+                                                                    coin in the list
+                                                                        """
+                                                                            n_coins = 0
+                                                                                if total <= 0:
+                                                                                            return n_coins
+                                                                                            coins.sort(reverse=True)
+                                                                                                while (total > 0 and coins):
+                                                                                                        n = int(total / coins[0])
+                                                                                                                total = total - (coins[0] * n)
+                                                                                                                        n_coins = n_coins + n
+                                                                                                                                coins.remove(coins[0])
+                                                                                                                                    if total != 0:
+                                                                                                                                            return -1
+                                                                                                                                                return n_coins
