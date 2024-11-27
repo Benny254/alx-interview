133000965d8bff365f4dfbdd89f6e4ad45aa1bb2
Benny254 <asshimbenny01@gmail.com>
2024-11-27 09:13:33 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""
-Task: Change comes from within
-Given a pile of coins of different values,
-determine the fewest number of coins needed to
-meet a given amount total
-"""
+"""Making Change Problem"""
 
 
 def makeChange(coins, total):
-        """
-            Given a pile of coins of different values, determine the fewest
-                number of coins needed to meet a given amount total.
-                    Return: fewest number of coins needed to meet total
-                            - If total is 0 or less, return 0
-                                    - If total cannot be met by any number of coins you have, return -1
-                                            - Coins is a list of the values of the coins in your possession
-                                                    - The value of a coin will always be an integer greater than 0
-                                                            - You can assume you have an infinite number of each denomination of
-                                                                    coin in the list
-                                                                        """
-                                                                            n_coins = 0
-                                                                                if total <= 0:
-                                                                                            return n_coins
-                                                                                            coins.sort(reverse=True)
-                                                                                                while (total > 0 and coins):
-                                                                                                        n = int(total / coins[0])
-                                                                                                                total = total - (coins[0] * n)
-                                                                                                                        n_coins = n_coins + n
-                                                                                                                                coins.remove(coins[0])
-                                                                                                                                    if total != 0:
-                                                                                                                                            return -1
-                                                                                                                                                return n_coins
+        """Making Changes"""
+            if total <= 0:
+                        return 0
+                        elif total > 0:
+                                    newList = sorted(coins[:])
+                                            newList = list(reversed(newList))
+                                                    count = 0
+                                                            value = total + 0
+                                                                    index = 0
+                                                                            while value >= 0 and (index < len(newList)):
+                                                                                            if value >= newList[index]:
+                                                                                                                value = value - newList[index]
+                                                                                                                                count += 1
+                                                                                                                                            elif value < newList[index]:
+                                                                                                                                                                index += 1
+                                                                                                                                                                        if index == len(newList):
+                                                                                                                                                                                        if value != 0:
+                                                                                                                                                                                                            return -1
+                                                                                                                                                                                                                    elif value == 0:
+                                                                                                                                                                                                                                        return count
