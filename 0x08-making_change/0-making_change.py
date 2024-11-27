036a79c9740be5ca46545c980e8292d2278756b2
Benny254 <asshimbenny01@gmail.com>
2024-11-27 09:19:58 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""Making Change Problem"""
-
+"""Making Change Problem.
+"""
 
 def makeChange(coins, total):
-        """Making Changes"""
-            if total <= 0:
-                        return 0
-                        elif total > 0:
-                                    newList = sorted(coins[:])
-                                            newList = list(reversed(newList))
-                                                    count = 0
-                                                            value = total + 0
-                                                                    index = 0
-                                                                            while value >= 0 and (index < len(newList)):
-                                                                                            if value >= newList[index]:
-                                                                                                                value = value - newList[index]
-                                                                                                                                count += 1
-                                                                                                                                            elif value < newList[index]:
-                                                                                                                                                                index += 1
-                                                                                                                                                                        if index == len(newList):
-                                                                                                                                                                                        if value != 0:
-                                                                                                                                                                                                            return -1
-                                                                                                                                                                                                                    elif value == 0:
-                                                                                                                                                                                                                                        return count
+        """Determines the fewest number of coins needed to meet a given amount total.
+
+            Args:
+                    coins (list): A list of the values of the coins in your possession.
+                            total (int): The total amount to be met.
+
+                                Returns:
+                                        int: The fewest number of coins needed to meet the total, or -1 if the total cannot be met.
+                                            """
+                                                if total <= 0:
+                                                            return 0
+
+                                                            sorted_coins = sorted(coins, reverse=True)
+                                                                count = 0
+                                                                    remaining = total
+
+                                                                        for coin in sorted_coins:
+                                                                                    while remaining >= coin:
+                                                                                                    remaining -= coin
+                                                                                                                count += 1
+
+                                                                                                                    return count if remaining == 0 else -1
+
