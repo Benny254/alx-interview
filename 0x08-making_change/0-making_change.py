a6980268ca95754d337f47a59b77d2360572e526
Benny254 <asshimbenny01@gmail.com>
2024-11-27 09:00:52 +0300

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""
-Interview Question on: fewest number of coins needed to
-meet a given amount total
-"""
+"""Making Change Problem"""
 
 
-def makeChange(coins, total):
-        """ fewest number of coins needed to meet total """
-            if total <= 0:
-                        return 0
-                        # sort the coins in descending order
-                            coins.sort(reverse=True)
-                                change = 0
-                                    for coin in coins:
-                                                if total <= 0:
-                                                                break
-                                                                    temp = total // coin
-                                                                            change += temp
-                                                                                    total -= (temp * coin)
-                                                                                        if total != 0:
-                                                                                                    return -1
-                                                                                                    return change
+def make_change(coins, total):
+        """Determines the fewest number of coins needed \
+                        to meet a given amount total"""
+                            if total <= 0:
+                                        return 0
+
+                                        current_total = 0
+                                            used_coins = 0
+                                                coins = sorted(coins, reverse=True)
+                                                    for coin in coins:
+                                                                r = (total - current_total) // coin
+                                                                        current_total += r * coin
+                                                                                used_coins += r
+                                                                                        if current_total == total:
+                                                                                                        return used_coins
+                                                                                                        return -1
