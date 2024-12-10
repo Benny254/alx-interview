3cf96fa3093872ec2459adc56c58064411d470ba
Benny254 <asshimbenny01@gmail.com>
2024-12-10 23:21:29 +0300

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,7 +6,7 @@ The player that cannot make a move loses the game.
 
 def isWinner(x, nums):
         """
-            Return: name of the player that won the most rounds.
+        Return: name of the player that won the most rounds.
                 If the winner cannot be determined, return `None`.
                     """
                         def is_prime(n):
