cf75c509a02456a92232f7af7879321fb0b18321
Benny254 <asshimbenny01@gmail.com>
2024-12-10 22:54:52 +0300

    update

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 """ Prime Game """
 
 
-def is_prime(n):
+def check_prime(n):
         """ Check if n is a prime number """
             for i in range(2, int(n ** 0.5) + 1):
                         if not n % i:
@@ -15,7 +16,7 @@ def is_prime(n):
                                                 last_prime = primes[-1]
                                                     if n > last_prime:
                                                                 for i in range(last_prime + 1, n + 1):
-                                                                                if is_prime(i):
+                                                                                if check_prime(i):
                                                                                                     primes.append(i)
                                                                                                                 else:
                                                                                                                                     primes.append(0)
@@ -25,6 +26,7 @@ def is_prime(n):
                                                                                                                                             """ x is the number of rounds and nums is an array of n
                                                                                                                                                 Return: name of the player that won the most rounds
                                                                                                                                                     If the winner cannot be determined, return None """
+
                                                                                                                                                         score = {"Maria": 0, "Ben": 0}
                                                                                                                                                             primes = [0, 0, 2]
                                                                                                                                                                 add_prime(max(nums), primes)
